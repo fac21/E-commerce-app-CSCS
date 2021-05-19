b0af04cf1f114190701e1b2cb2eced7e2109b0b3
chisha <aamaechi@hotmail.com>
2021-05-19 11:50:07 +0100

    Updating database
    Co-authored-by: sevdas <sshukrieva@gmail.com>
    Co-authored-by: Craig MacLeod <c92macleod@gmail.com>
    Co-authored-by: sakireid <59439482+haginom@users.noreply.github.com>
    Co-authored-by: Chisha <64862888+NewCoderCA@users.noreply.github.com>

--- database/init.sql
+++ database/init.sql
@@ -1,20 +1,8 @@
 BEGIN;
 
-DROP TABLE IF EXISTS users, sessions, products, userbasket CASCADE;
+DROP TABLE IF EXISTS products, userbasket CASCADE;
 
 SET timezone = 'Europe/London';
-
-CREATE TABLE users (
-  id serial primary key,
-  name varchar(255) NOT NULL,
-  email text unique not NULL,
-  password text not null
-);
-
-CREATE TABLE sessions (
-    sid text primary key,
-    data json not null
-);
 
 CREATE TABLE products(
     id serial primary key,
@@ -34,14 +22,14 @@ CREATE TABLE userbasket(
 );
 
 
-INSERT INTO users (name, email, password) VALUES
-('candy','candy@hotmail.com','candy123');
+INSERT INTO products (name, description, price, vegan, image, instock) VALUES
+('Burberry Muffin', 'blueberry muffin with gold trimmings', 17, true, 'https://images.unsplash.com/photo-1516918977792-f0f8948aa15a?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=668&q=80', true),
+('Dolce and Banana', 'banana doughnut, gold trimmings, chocolate sprinkles', 17, true, 'http://kaleforniakravings.com/wp-content/uploads/2020/02/Banana-pancake-donuts-4-819x1024.jpg', true),
+('Choco Chanel', 'chocolate glazed doughnut, caramel, gold sprinkles', 17, true, 'https://www.gretchensveganbakery.com/wp-content/uploads/2018/09/turtle-donuts-feature-resized-2-1024x683.jpg', true),
+('Chewy Vuitton', 'chewy and toffee, gold dusted, caramel infused', 17, true, 'https://i.redd.it/d0kz5bs45xz61.jpg', true),
+('Donutella Versace', 'nutella twisted, gold dusted, caramel infused chocolagte doughnut', 17, true, 'https://media4.s-nbcnews.com/j/newscms/2016_01/926331/gold-donut-today-160107-tease_feddb4a70c5cd0b9492ea1122b43a357.social_share_1024x768_scale.jpg', true),
+('Giorgio Almondi', 'almond and chocolate doughnut, caramel, gold swirl', 17, true, 'https://d27hwuc9z2lofn.cloudfront.net/wp-content/uploads/2014/04/White-Chocolate-Almond-Baked-Donuts.jpg', true);
 
-INSERT INTO sessions (sid, data) VALUES
-('abc123','{"test":"stuff"}');
-
-INSERT INTO products (name, description, price, vegan, instock) VALUES
-('Burberry Muffin', 'blueberry muffin with gold trimmings', 17, true, true);
 
 
 
