d2e9d524e5ca6696f139a955520f6a5bd2a48bcb
sevdas <sshukrieva@gmail.com>
2021-05-19 11:04:13 +0100

    add data to the databse - stretch Refers #12
    Co-authored-by: sakireid <59439482+haginom@users.noreply.github.com>

--- pages/sign-up.js
+++ pages/sign-up.js
@@ -1,29 +1,75 @@
 import Link from "next/link";
+import React, { useState } from "react";
 
-export default function SignUpPage() {
-  const registerUser = (event) => {
-    event.preventDefault();
-    console.log("Hello");
-    console.log(event);
-  };
+//How do you access user values inside React Component?
+// Working with multiple inputs
+
+// export default function SignUpPage() {
+// const registerUser = (event) => {
+//   event.preventDefault();
+//   console.log("Hello");
+//   console.log(event);
+// };
+// const [name, setName] = useState("");
+// const registerUser = (event) => {
+//   console.log(event);
+//   setName(event.target.value);
+// };
+
+function MyControlledInput({}) {
+  const [values, setValues] = useState({
+    name: "",
+    password: "",
+    email: "",
+  });
+
+  //Single input
+  // const onChange = (event) => {
+  //   setName(event.target.value);
+  // };
+
+  // const handleChange = ({ target }) => {
+  //   setState({ [target.name]: target.value });
+  // };
 
   return (
     <div className="sign-up__form">
       <h1>Sign up</h1>
       <form>
+        {/* <div>Input value: {value}</div>
+        <input value={value} onChange={onChange} /> */}
+
         <label htmlFor="name">
           Name <span aria-hidden="true"></span>
         </label>
-        <input type="name" id="name" name="name" required />
+        <input
+          type="name"
+          id="name"
+          name="name"
+          value={values.name}
+          onChange={MyControlledInput}
+          required
+        />
 
         <label htmlFor="email">
           Email <span aria-hidden="true"></span>
         </label>
-        <input type="email" id="email" name="email" required />
+        <input
+          type="email"
+          id="email"
+          name="email"
+          value={values.email}
+          onChange={MyControlledInput}
+          required
+        />
 
         <label htmlFor="password">
           Password <span aria-hidden="true">*</span>
         </label>
+        {/* <div id="passwordRequirements">
+          Passwords must contain at least one letter and one number, and contain
+          at least 8 characters.
+        </div> */}
         <input
           type="password"
           id="password"
@@ -32,8 +78,10 @@ export default function SignUpPage() {
           // pattern=".*\d.*"
           // minlength="8"
           // required
+          value={values.password}
+          onChange={MyControlledInput}
         />
-        <button onClick={registerUser}>Sign Up</button>
+        <button>Sign Up</button>
       </form>
       <br></br>
       <Link href="/">
@@ -42,3 +90,5 @@ export default function SignUpPage() {
     </div>
   );
 }
+
+export default MyControlledInput;
