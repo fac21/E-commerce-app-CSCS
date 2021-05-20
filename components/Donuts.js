0c9f3ce1c13aeac9c292c949ac6906c5afa1b818
chisha <aamaechi@hotmail.com>
2021-05-20 13:05:54 +0100

    Adding CSS to Header

--- components/Donuts.js
+++ components/Donuts.js
@@ -9,19 +9,28 @@ export default function Donuts({ data, basket }) {
 
   const donutArray = data.map((donut) => {
     return (
-      <div key={donut.id}>
-        <h1 className="donut__name">{donut.name}</h1>
-        <img
-          className="donut__image"
-          src={donut.image}
-          alt={"picture of " + donut.name + " donut"}
-        />
-        <p className="donut__description">{donut.description}</p>
-        <p className="donut__price">£{donut.price.toFixed(2)}</p>
-        <button className={donut.name} onClick={addDonut}>
-          Gimme a Donut
-        </button>
-      </div>
+      <section>
+        <ul>
+          <li>Vegan</li>
+          <li>Min Price</li>
+          <li>Max Price</li>
+        </ul>
+        <hr></hr>
+
+        <div key={donut.id}>
+          <h1 className="donut__name">{donut.name}</h1>
+          <img
+            className="donut__image"
+            src={donut.image}
+            alt={"picture of " + donut.name + " donut"}
+          />
+          <p className="donut__description">{donut.description}</p>
+          <p className="donut__price">£{donut.price.toFixed(2)}</p>
+          <button className={donut.name} onClick={addDonut}>
+            Gimme a Donut
+          </button>
+        </div>
+      </section>
     );
   });
   return <>{donutArray}</>;
